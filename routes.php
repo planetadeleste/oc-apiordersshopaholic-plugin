793f59bd72b9d237795796b2c0dc799390ef6773
Alvaro Canepa <bfpdevel@gmail.com>
2025-11-09 10:41:51 -0300

    Eliminar el middleware 'bindings' de la ruta API v1

--- routes.php
+++ routes.php
@@ -1,7 +1,7 @@
 <?php
 Route::prefix('api/v1')
     ->namespace('PlanetaDelEste\ApiOrdersShopaholic\Controllers\Api')
-    ->middleware(['throttle:120,1', 'bindings'])
+    ->middleware(['throttle:120,1'])
     ->group(
         function () {
             $sPath = plugins_path('planetadeleste/apiordersshopaholic/routes');
